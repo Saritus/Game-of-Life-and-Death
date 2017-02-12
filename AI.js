c4e89e56acb87497f61de040c71fdc68c15e5656
Sebastian Mischke <SebastianMischke@gmx.de>
2017-02-12 23:12:46 +0100

    Implement one extra calculation step

--- AI.js
+++ AI.js
@@ -3,9 +3,11 @@ function AI(player) {
   this.player = player;
 
   this.getMove = function(field) {
+    var startratio = field.getRatio();
     var start = new Date().getTime();
     var move = [];
     var best_ratio;
+    var worst_ratio;
     var new_field;
 
     for (var x = 0; x < field.cols; x++) {
@@ -16,16 +18,44 @@ function AI(player) {
           continue;
         }
         new_field.step();
-
         var new_ratio = new_field.getRatio();
 
-        if ((!best_ratio) ||
-          (this.player == 1 && new_ratio > best_ratio) ||
-          (this.player == 2 && new_ratio < best_ratio)) {
-          best_ratio = new_ratio;
+        for (var x1 = 0; x1 < field.cols; x1++) {
+          for (var y1 = 0; y1 < field.rows; y1++) {
+
+            var other_field = new_field.clone();
+            var other_player;
+            if (this.player == 1) {
+              other_player = 2;
+            } else {
+              other_player = 1;
+            }
+
+            if (!other_field.click(other_player, x1, y1)) {
+              continue;
+            }
+            other_field.setNeighbors();
+            other_field.step();
+            var other_ratio = other_field.getRatio();
+
+            if ((!best_ratio) ||
+              (other_player == 1 && other_ratio > best_ratio) ||
+              (other_player == 2 && other_ratio < best_ratio)) {
+              best_ratio = other_ratio;
+              //move['x'] = x;
+              //move['y'] = y;
+            }
+          }
+        }
+
+        if ((!worst_ratio) ||
+          (this.player == 1 && best_ratio > worst_ratio) ||
+          (this.player == 2 && best_ratio < worst_ratio)) {
+          worst_ratio = best_ratio;
           move['x'] = x;
           move['y'] = y;
         }
+
       }
     }
     //console.log("best_ratio", best_ratio);

ed82c87958a4a56b36d945e06e2a3ba2cb1b4720
Sebastian Mischke <SebastianMischke@gmx.de>
2017-02-12 11:45:48 +0100

    Add type to Cell

--- Cell.js
+++ Cell.js
@@ -2,10 +2,25 @@ function Cell(x, y, w) {
     this.x = x;
     this.y = y;
     this.w = w;
+    this.type = floor(random(0, 3));
 
     this.draw = function() {
         noStroke();
-        fill(255, 0, 0);
+
+        switch (this.type) {
+            case 0:
+                fill(0, 0, 0);
+                break;
+            case 1:
+                fill(255, 0, 0);
+                break;
+            case 2:
+                fill(0, 0, 255);
+                break;
+            default:
+                fill(0, 255, 0);
+        }
+
         rect(this.x * this.w + 1, this.y * this.w + 1, this.w - 1, this.w - 1);
     }
 }

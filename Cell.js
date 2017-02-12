77cd6f66e136eaa2f8da3161889abc4bb7b44838
Sebastian Mischke <SebastianMischke@gmx.de>
2017-02-12 12:32:56 +0100

    Add getNextStep

--- Cell.js
+++ Cell.js
@@ -9,7 +9,6 @@ function Cell(x, y, w) {
   this.neighborTypeTwo = 0;
 
   this.draw = function() {
-
     switch (this.type) {
       case 0:
         fill(255);
@@ -25,5 +24,27 @@ function Cell(x, y, w) {
     }
 
     rect(this.x * this.w, this.y * this.w, this.w, this.w);
+  }
+
+  this.getNextStep = function() {
+    if (this.type) {
+      if (this.neighborCount < 2) {
+        return 0;
+      } else if (this.neighborCount > 3) {
+        return 0;
+      } else {
+        return this.type;
+      }
+    } else {
+      if (neighborCount == 3) {
+        if (this.neighborTypeOne > this.neighborTypeTwo) {
+          return 1;
+        } else {
+          return 2;
+        }
+      } else {
+        return 0;
+      }
+    }
   }
 }

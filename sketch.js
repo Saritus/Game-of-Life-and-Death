6c3fe2d3a121be452e32883872bd29a5291b23d2
Sebastian Mischke <SebastianMischke@gmx.de>
2017-02-12 13:23:48 +0100

    Add output

--- sketch.js
+++ sketch.js
@@ -3,12 +3,15 @@ var rows;
 var field;
 var w = 30;
 var player = 1;
+var player_output;
 
 function setup() {
   createCanvas(601, 601);
   cols = floor(width / w);
   rows = floor(height / w);
-  frameRate(20);
+
+  player_output = createP();
+  player_output.html("Spieler " + player + " ist am Zug");
 
   field = new Field(rows, cols, w);
   field.setNeighbors();
@@ -22,9 +25,15 @@ function draw() {
 function mouseClicked() {
   var x = floor(mouseX / w);
   var y = floor(mouseY / w);
+
+  if (!field.cells[x][y]) {
+    return;
+  }
+
   if (field.cells[x][y].type) {
     field.cells[x][y].type = 0;
   } else {
+    return;
     field.cells[x][y].type = player;
   }
 
@@ -37,4 +46,5 @@ function mouseClicked() {
   } else {
     player = 1;
   }
+  player_output.html("Spieler " + player + " ist am Zug");
 }

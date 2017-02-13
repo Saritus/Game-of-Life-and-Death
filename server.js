14dafb34afccfa4b50f3ff04687e97b7562461a6
Sebastian Mischke <SebastianMischke@gmx.de>
2017-02-13 21:53:09 +0100

    Send index.html to user

--- server.js
+++ server.js
@@ -10,3 +10,9 @@ app.configure(function() {
   // statische Dateien ausliefern
   app.use(express.static(__dirname + '/public'));
 });
+
+// wenn der Pfad / aufgerufen wird
+app.get('/', function(req, res) {
+  // so wird die Datei index.html ausgegeben
+  res.sendfile(__dirname + '/public/index.html');
+});

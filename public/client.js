1d83865bb60b97b5cb7551b6e7604ddbe45fde70
Sebastian Mischke <SebastianMischke@gmx.de>
2017-02-13 22:12:34 +0100

    Add receive function

--- public/client.js
+++ public/client.js
@@ -1,3 +1,23 @@
 $(document).ready(function() {
   var socket = io.connect();
+
+  socket.on('chat', function(data) {
+    var zeit = new Date(data.zeit);
+    $('#content').append(
+      $('<li></li>').append(
+        // Uhrzeit
+        $('<span>').text('[' +
+          (zeit.getHours() < 10 ? '0' + zeit.getHours() : zeit.getHours()) +
+          ':' +
+          (zeit.getMinutes() < 10 ? '0' + zeit.getMinutes() : zeit.getMinutes()) +
+          '] '
+        ),
+        // Name
+        $('<b>').text(typeof(data.name) != 'undefined' ? data.name + ': ' : ''),
+        // Text
+        $('<span>').text(data.text))
+    );
+    // nach unten scrollen
+    $('body').scrollTop($('body')[0].scrollHeight);
+  });
 });
